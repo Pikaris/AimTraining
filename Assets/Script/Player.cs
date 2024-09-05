a07de456c44d18f081e3cfbd69bdf376180cdc10
tesia102 <koworihime@gmail.com>
2024-09-05 13:50:49 +0900

    20240905

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,21 +11,33 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float mouseSensitivityX = 3.0f;
+    [SerializeField] float mouseSensitivityY = 2.0f;
+
+    float limitMinX = -60.0f;
+    float limitMaxX = 60.0f;
+
     public float moveSpeed = 5.0f;
 
     PlayerInputAction inputAction;
     Rigidbody rigid;
 
-    Vector2 aim;
+    CinemachineVirtualCamera vcam;
+
+    Vector2 mousePosition;
 
     Vector3 worldPosition;
     Vector3 movePosition;
+
+    
     
 
     private void Awake()
     {
         inputAction = new PlayerInputAction();
         rigid = GetComponent<Rigidbody>();
+        Transform child = transform.GetChild(2);
+        vcam = child.GetComponent<CinemachineVirtualCamera>();
         //movePosition = GetComponent<Vector3>();
 
         //direction = Vector3.zero;
@@ -53,12 +65,12 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rigid.Move(rigid.position + Time.fixedDeltaTime * moveSpeed * movePosition, Quaternion.identity);
+        rigid.Move(rigid.position + Time.fixedDeltaTime * moveSpeed * movePosition, vcam.transform.rotation);
     }
 
     private void Update()
     {
-        worldPosition = Camera.main.ScreenToWorldPoint(aim);
+        worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
     }
 
     private void OnMove(InputAction.CallbackContext context)
@@ -68,11 +80,14 @@ public class Player : MonoBehaviour
     }
     private void OnAim(InputAction.CallbackContext context)
     {
-        aim = Mouse.current.position.ReadValue();
+        mousePosition.x += Mouse.current.position.x.ReadValue() * mouseSensitivityX;
+        mousePosition.y -= Mouse.current.position.y.ReadValue() * mouseSensitivityY;
+        mousePosition.x = Mathf.Clamp(mousePosition.x, limitMinX, limitMaxX);
     }
+
     private void OnFire(InputAction.CallbackContext context)
     {
-        Ray ray = Camera.main.ScreenPointToRay(aim);
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
     }
 
     private void DoShake()
